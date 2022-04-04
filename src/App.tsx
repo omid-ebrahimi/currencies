0aed59c8d586ca4251e5e067d5fd15787bea95e1
Omid Ebrahimi <omid.ebrahimi.pro@gmail.com>
2022-04-04 14:40:33 +0430

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <Text>Hello!</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
