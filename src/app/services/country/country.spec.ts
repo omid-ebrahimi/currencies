cd7db863e8277f5de9063cf45c621e3edf363b51
Omid Ebrahimi <omid.ebrahimi.pro@gmail.com>
2022-04-19 17:17:11 +0430

    refactors tests and add some more tests for utils

--- src/app/services/country/country.spec.ts
+++ src/app/services/country/country.spec.ts
@@ -1,21 +1,63 @@
-import { Currency } from '@src/app/App.types';
-import { transformCurrency } from '@src/app/services/country/country.utils';
+import { Country, Currency } from '@src/app/App.types';
 import { CountryDto } from './country.types';
+import { transformCountry, transformCurrency } from './country.utils';
 
-describe('transformCurrency', () => {
-  it('transforms currencies field in country dto to currency', () => {
-    const currencies: CountryDto['currencies'] = {
-      IRR: {
-        name: 'Iranian Rials',
-        symbol: 'ریال',
-      },
-    };
+describe('utils', () => {
+  describe('transformCurrency()', () => {
+    it('given CountryDto.currencies, returns Currency', () => {
+      const currenciesDto: CountryDto['currencies'] = {
+        IRR: {
+          name: 'Iranian Rials',
+          symbol: 'ریال',
+        },
+      };
 
-    const currency: Currency | undefined = transformCurrency(currencies);
+      const currency = transformCurrency(currenciesDto);
 
-    expect(currency).toStrictEqual({
-      code: 'IRR',
-      ...currencies.IRR,
+      expect(currency).toStrictEqual<Currency>({
+        code: 'IRR',
+        ...currenciesDto.IRR,
+      });
+    });
+    it('given undefined, returns undefined', () => {
+      const currencies: CountryDto['currencies'] = undefined;
+      const currency = transformCurrency(currencies);
+      expect(currency).toBe(undefined);
+    });
+    it('given an empty object, returns undefined', () => {
+      const currencies: CountryDto['currencies'] = {};
+      const currency = transformCurrency(currencies);
+      expect(currency).toBe(undefined);
+    });
+  });
+  describe('transformCountry()', () => {
+    it('given CountryDto, returns Country', () => {
+      const countryDto: CountryDto = {
+        name: { common: 'Iran' },
+        capital: ['Tehran'],
+        currencies: {
+          IRR: {
+            name: 'Iranian Rials',
+            symbol: 'ریال',
+          },
+        },
+        flag: '🇮🇷',
+        population: 85000000,
+      };
+
+      const country = transformCountry(countryDto);
+
+      expect(country).toStrictEqual<Country>({
+        name: countryDto.name.common,
+        flag: countryDto.flag,
+        population: countryDto.population,
+        capital: countryDto.capital[0],
+        currency: {
+          code: 'IRR',
+          name: 'Iranian Rials',
+          symbol: 'ریال',
+        },
+      });
     });
   });
 });
--- src/utils/number/number.spec.ts
+++ src/utils/number/number.spec.ts
@@ -1,21 +1,25 @@
 import { formatWithCommas, numberWithCommas } from './number';
 
-describe('numberWithCommas', () => {
-  it('format integers correctly', () => {
+describe('numberWithCommas()', () => {
+  it('given an integer, returns comma-separated string', () => {
     const value = 12569845;
     const result = numberWithCommas(value);
     expect(result).toBe('12,569,845');
   });
-
-  it('format currencies correctly', () => {
+  it('given a currency with 2 decimal points, returns comma-separated currency', () => {
     const value = 12569.25;
     const result = numberWithCommas(value);
     expect(result).toBe('12,569.25');
   });
 });
 
-describe('formatWithCommas', () => {
-  it("doesn't touch none number values", () => {
+describe('formatWithCommas()', () => {
+  it('given a string that can be converted to number, returns a comma-separated string', () => {
+    const value = '12345.256';
+    const result = formatWithCommas(value);
+    expect(result).toBe('12,345.256');
+  });
+  it("'given a string that can't be converted to number, returns the given string'", () => {
     const value = 'Unknown';
     const result = formatWithCommas(value);
     expect(result).toBe('Unknown');
